a2e6762d3b403ad350661bfc53f99caa395ca752
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 18:21:09 +0530

    furst commit

---
+++ day 7/src/TypeCasting.java
@@ -0,0 +1,13 @@
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			
+		       int i= 10;  // variable with f is a type of integer which is holding value of 10
+		       long l = (long) i;  defining a var with name l which of datatype long = convert value that is present in I to long
+		       System.out.println(l);// TODO Auto-generated method stub
+
+	}
+
+}
