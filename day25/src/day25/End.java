46f4d4bf7dca1cf1ea5a0da78095a618228438df
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-23 00:08:39 +0530

    first

---
+++ day25/src/day25/End.java
@@ -0,0 +1,18 @@
+package day25;
+
+public class End {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; j <= i; j++) 
+			{
+				System.out.print('@');
+			}
+			System.out.println();
+		}
+// TODO Auto-generated method stub
+
+	}
+
+}
