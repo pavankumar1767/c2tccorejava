9878eabedfcf104c6a744bcd2848c9da6e0e76d2
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-23 00:02:40 +0530

    first

---
+++ day22/src/day22/FWD.java
@@ -0,0 +1,22 @@
+package day22;
+
+public class FWD {
+
+	public static void main(String[] args) {
+		for(int i=1,j=1; i<10||j<10;i++,j++)
+		  { 
+			  j++;
+			  System.out.println("i="+i+" and j="+j); 
+		  }
+			  int i=1,j=1;
+			  while(i<10 || j<10)
+			  { 
+				  i++; j++;
+			      System.out.println("i="+i+" and j="+j); 
+			  }	
+			
+// TODO Auto-generated method stub
+
+	}
+
+}
