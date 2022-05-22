a650585c3695d07edba21263c53df739a1ca1bc1
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 23:38:44 +0530

    first

---
+++ day14/src/day14/IfElse.java
@@ -0,0 +1,20 @@
+package day14;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int x = 5;
+
+		
+		 if (x == 5) 
+		  { 
+		  if (x >= 5) 
+		  {
+		  	System.out.println("X is greater than 5"); 
+		   }
+		  else 
+		  {
+		  System.out.println("X is smaller than 5"); 
+		  } 
+		 }
+	}
+}
