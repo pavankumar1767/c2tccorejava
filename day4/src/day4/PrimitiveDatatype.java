e9d3e28303430a02859fa919f9e2132d3e694ea9
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 17:48:11 +0530

    first commit

---
+++ day4/src/day4/PrimitiveDatatype.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class PrimitiveDatatype {
+
+	public static void main(String[] args) {
+	    int marker = 512;
+	    double percentage = marker * 0.46f;
+	    
+	    System.out.println("percentage:"+percentage);// TODO Auto-generated method stub
+
+	}
+
+}
