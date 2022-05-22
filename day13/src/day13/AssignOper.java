601bd0494e839ea8c8566c48b87df753d706aabd
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 23:35:01 +0530

    first

---
+++ day13/src/day13/AssignOper.java
@@ -0,0 +1,12 @@
+package day13;
+
+public class AssignOper {
+
+	public static void main(String[] args) {
+		int x = 4;
+		x %= 5;
+		System.out.println(x);// TODO Auto-generated method stub
+
+	}
+
+}
