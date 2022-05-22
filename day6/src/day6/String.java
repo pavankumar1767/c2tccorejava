2bb61a4746d95d176d969985dd5125f25a2a75ab
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 18:11:43 +0530

    first commit

---
+++ day6/src/day6/String.java
@@ -0,0 +1,11 @@
+package day6;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		String var = new String("Hello world");
+		System.out.println(var);// TODO Auto-generated method stub
+
+	}
+
+}
