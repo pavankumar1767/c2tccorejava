4e2e0040d15efb627560a8f80d2a5cf15bf43b85
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 23:54:02 +0530

    first

---
+++ day20/src/day20/WhileLoop.java
@@ -0,0 +1,16 @@
+package day20;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+// TODO Auto-generated method stub
+
+	}
+
+}
