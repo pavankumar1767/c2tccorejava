cb80dac839267878a18b45d92a979adf576f8fcd
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 18:01:33 +0530

    first commit

---
+++ day5/src/day5/BooleanUnicode.java
@@ -0,0 +1,14 @@
+package day5;
+
+public class BooleanUnicode {
+
+	public static void main(String[] args) {
+		boolean var = false;
+		System.out.println(var);
+
+		char var1 = '\u00A7';
+		System.out.println(var1);// TODO Auto-generated method stub
+
+	}
+
+}
