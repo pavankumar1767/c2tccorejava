773478f4bdeef1ebaa5b22b20f70453f5a971607
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 23:31:56 +0530

    first

---
+++ day12/src/day12/TernaryOper.java
@@ -0,0 +1,14 @@
+package day12;
+
+public class TernaryOper {
+
+	public static void main(String[] args) {
+int x = 10;
+		
+		x = (10 == x) ? 1 : 0;   //  condition ?: True:False
+		
+		System.out.println(x);// TODO Auto-generated method stub
+
+	}
+
+}
