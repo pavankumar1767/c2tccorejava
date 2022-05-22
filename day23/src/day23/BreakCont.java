8b06f34801b7d4f29ac1554d315f2fb219b04a17
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-23 00:04:24 +0530

    first

---
+++ day23/src/day23/BreakCont.java
@@ -0,0 +1,18 @@
+package day23;
+
+public class BreakCont {
+
+	public static void main(String[] args) {
+        for(int i=1; i<=10; i++)
+        {
+       	 if(i == 1)
+       	 {
+       		 continue;   // break;
+       	 }
+       	 System.out.println(i);
+        }
+// TODO Auto-generated method stub
+
+	}
+
+}
