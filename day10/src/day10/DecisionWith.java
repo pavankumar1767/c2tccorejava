ddf2ac9bec8212ac4b474966609c832e8f71ca0b
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 23:25:37 +0530

    first

---
+++ day10/src/day10/DecisionWith.java
@@ -0,0 +1,19 @@
+package day10;
+
+public class DecisionWith {
+
+	public static void main(String[] args) {
+		int x = 8, y = 7;
+		
+        if(x >= y)
+        {
+     	   System.out.println("true");
+        }
+        else
+        {
+     	   System.out.println("false");
+        }// TODO Auto-generated method stub
+
+	}
+
+}
