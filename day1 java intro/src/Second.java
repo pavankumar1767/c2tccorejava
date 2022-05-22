58f64ae7cd45366e5e6e2f3d5313f80a9be1083e
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 10:04:34 +0530

    first commit

---
+++ day1 java intro/src/Second.java
@@ -0,0 +1,13 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+		int x;
+		x=3;
+		double y=5.5;
+		System.out.println("x is"+x);
+		System.out.println("y is"+y);// TODO Auto-generated method stub
+
+	}
+
+}
