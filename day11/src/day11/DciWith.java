c1eccc254a7590408bbc7240463c3f2a020c421e
asritha ganisetti <asritha ganisetti@LAPTOP-NUKBH5AB>
2022-05-22 23:29:27 +0530

    first

---
+++ day11/src/day11/DciWith.java
@@ -0,0 +1,19 @@
+package day11;
+
+public class DciWith {
+
+	public static void main(String[] args) {
+		 int x = 10; int y = 11;
+		  
+		  if(!(x < y) || (x == y))
+		  { 
+			  System.out.println("Condition is TRUE");
+		  }
+		  else
+		  { 
+			  System.out.println("Condition is FALSE"); 
+		}// TODO Auto-generated method stub
+
+	}
+
+}
